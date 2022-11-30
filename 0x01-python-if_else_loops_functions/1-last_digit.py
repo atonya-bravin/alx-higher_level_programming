6ce1207a15f1a922dee73e04c463322235f5f939
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-29 18:48:00 -0800

    create string then format if using .format function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,10 @@ if number < 0:
 else:
     last_digit = number % 10
 if last_digit > 5:
-    print("Last digit of " + str(number) + " is " +
-            str(last_digit) + " and is greater than 5")
+    str = 'Last digit of {0} is {1} and is greater than 5'
 elif last_digit == 0:
-    print("Last digit of " + str(number) + " is " +
-            str(last_digit) + " and is 0")
+    str = 'Last digit of {0} is {1} and is 0'
 elif last_digit < 6 and last_digit != 0:
-    print("Last digit of " + str(number) + " is " +
-            str(last_digit) + " and is less than 6 and not 0")
+    str = 'Last digit of {0} is {1} and is less than 6 and not 0'
+
+print(str.format(number, last_digit))
