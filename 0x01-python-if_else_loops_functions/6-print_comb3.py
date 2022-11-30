fec799e03e435eb58b544eea5846509ce6166079
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-30 05:50:34 -0800

    number combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for outer_number in range(0, 9 ):
+    for inner_number in range((outer_number + 1), 10):
+        if outer_number == 8:
+            str = '{0}{1}'
+        else:
+            str = '{0}{1}, '
+        print(str.format(outer_number, inner_number), end="")
+print();
