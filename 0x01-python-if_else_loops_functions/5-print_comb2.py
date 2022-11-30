dbb3d33b274206704c5dfed1c79ca055db6f69ac
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-29 20:02:56 -0800

    print numbers from 0 - 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number != 99:
+        if number < 10:
+            str = '0{0}, '
+        else:
+            str = '{0}, '
+    elif number == 99:
+        str = '{0}\n'
+    print(str.format(number), end = "")
