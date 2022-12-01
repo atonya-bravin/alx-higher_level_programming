6dcfb6f6112739d23125cc288aedf6bb174d8454
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-01 12:07:40 -0800

    addition of command line argument values

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0;
+    for index in range(1, len(sys.argv)):
+        total += int((sys.argv)[index])
+
+    print("{}".format(total))
