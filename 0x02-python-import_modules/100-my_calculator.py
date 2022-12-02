9b636a610ff55d9b0c9f5e3cf019eb736039dfd9
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-01 18:42:29 -0800

    styling correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    print(len(sys.argv))
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operator = sys.argv[2]
-    a = sys.argv[1]
-    b = sys.argv[3]
-    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
+    operator = argv[2]
+    a = argv[1]
+    b = argv[3]
+    if operator != '+' and operator != '-' and
+    operator != '*' and operator != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     if operator == '+':
-        print("{0} {1} {2} = {3}".format(a, operator, b, add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{0} {1} {2} = {3}".format(a, operator, b, add(int(a), int(b))))
         exit(0)
     elif operator == '-':
-        print("{0} {1} {2} = {3}".format(a, operator, b, sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{0} {1} {2} = {3}".format(a, operator, b, sub(int(a), int(b))))
         exit(0)
     elif operator == '*':
-        print("{0} {1} {2} = {3}".format(a, operator, b, mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{0} {1} {2} = {3}".format(a, operator, b, mul(int(a), int(b))))
         exit(0)
     elif operator == '/':
-        print("{0} {1} {2} = {3}".format(a, operator, b, div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{0} {1} {2} = {3}".format(a, operator, b, div(int(a), int(b))))
         exit(0)
