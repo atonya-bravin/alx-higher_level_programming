7bd7777a0575c5ff30723cccc0aa70a02c365809
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-02 22:43:36 -0800

    0-print_list_integer.py: change the output from the index to elements in the list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for index in range(len(my_list)):
-        print("{0}".format(my_list[index]))
+    for element in my_list:
+        print("{0}".format(element))
