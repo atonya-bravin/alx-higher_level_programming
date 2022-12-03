6d2d16b179fcc25006eff6c39485c2c9476d7b79
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-03 00:45:18 -0800

    6-print_matrix_integer.py: consider end of string if we are printing the last element do not print space:

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for outter_idx in range(len(matrix)):
             for inner_idx in range(len(matrix[outter_idx])):
-                print("{:d}".format(matrix[outter_idx][inner_idx]), end=" ")
+                if inner_idx == (len(matrix[outter_idx]) - 1):
+                    print("{:d}".format(matrix[outter_idx][inner_idx]), end="")
+                else:
+                    print("{:d}".format(matrix[outter_idx][inner_idx]), end=" ")
             print()
