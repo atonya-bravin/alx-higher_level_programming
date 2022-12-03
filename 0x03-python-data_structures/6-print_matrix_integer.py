4b31b1d896a0fdae9a91dd88ea69c3712421cc89
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-03 00:32:33 -0800

    6-print_matrix_integer.py: check for empty lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for outter_index in range(len(matrix)):
-        for inner_index in range(len(matrix[outter_index])):
-            print("{:d}".format(matrix[outter_index][inner_index]), end=" ")
-        print()
+    cariage_return = "\n"
+    if matrix:
+        for outter_index in range(len(matrix)):
+            for inner_index in range(len(matrix[outter_index])):
+                print("{:d}".format(matrix[outter_index][inner_index]), end=" ")
+            print("{}".foramt(cariage_return))
