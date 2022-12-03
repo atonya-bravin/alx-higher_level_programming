e08a28d8f7f0dc39f1bf6b99490c113c9edaafb4
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-03 00:49:05 -0800

    6-print_matrix_integer.py: correction on styling

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for outter_idx in range(len(matrix)):
-            for inner_idx in range(len(matrix[outter_idx])):
-                if inner_idx == (len(matrix[outter_idx]) - 1):
-                    print("{:d}".format(matrix[outter_idx][inner_idx]), end="")
+        for outer_idx in range(len(matrix)):
+            for inner_idx in range(len(matrix[outer_idx])):
+                if inner_idx == (len(matrix[outer_idx]) - 1):
+                    print("{:d}".format(matrix[outer_idx][inner_idx]), end="")
                 else:
-                    print("{:d}".format(matrix[outter_idx][inner_idx]), end=" ")
+                    print("{:d}".format(matrix[outer_idx][inner_idx]), end=" ")
             print()
