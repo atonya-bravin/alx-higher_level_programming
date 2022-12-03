4aa598c2875e5a202b9dcfe76cb9404a8b1f2d1e
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-03 00:24:59 -0800

    use {:d} to format strings

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for outter_index in range(len(matrix)):
         for inner_index in range(len(matrix[outter_index])):
-            print("{}".format(matrix[outter_index][inner_index]), end=" ")
+            print("{:d}".format(matrix[outter_index][inner_index]), end=" ")
         print()
