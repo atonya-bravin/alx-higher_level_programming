09af08c373353a0c92da66ca81f2797c03a3e064
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-04 17:31:02 -0800

    7-add_tuple.py: change tuple value from method

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = validate_tuple(tuple_a)
+    tuple_b = validate_tuple(tuple_b)
 
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+
+
+def validate_tuple(test_tuple=()):
+    if len(test_tuple) < 2:
+        if len(test_tuple) == 1:
+            test_tuple = (test_tuple[0], 0)
+        elif len(test_tuple) == 0:
+            test_tuple = (0, 0)
+        elif len(test_tuple) > 2:
+            test_tuple = (test_tuple[0], test_tuple[1])
+
+    return test_tuple
