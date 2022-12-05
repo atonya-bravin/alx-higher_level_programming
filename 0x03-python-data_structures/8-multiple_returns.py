05fce87315168027deee8052b4eb4f369a491861
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-04 17:34:46 -0800

    8-multiple_returns.py: the tuple of an empty string contains length 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        return (0, None)
     else:
         sentence_information = ((len(sentence)), sentence[0])
         return sentence_information
