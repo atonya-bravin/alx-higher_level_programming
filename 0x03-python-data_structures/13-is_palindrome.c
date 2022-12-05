a89977794e6333a8bcbc9370e5640ac7999d81bf
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-05 09:30:39 -0800

    13-is_palindrome.c: check whether the head is equal to NULL

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks whether a linked list is empty
+ *
+ * @head: the head of the linked list
+ *
+ * Return: 0(not empty) or 1(empty)
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+		return (1);
+	return (0);
+}
