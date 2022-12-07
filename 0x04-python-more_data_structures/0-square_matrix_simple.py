93ce498c0860e037ab31c6c95fabe29a0344a234
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-07 11:26:26 -0800

    0-square_matrix_simple.py: correction of first line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for outter_index in range(len(matrix)):
         product_matrix = []
-        for inner_index in range(len(matrix[outter_index])):           
+        for inner_index in range(len(matrix[outter_index])):
             inner_matrix = matrix[outter_index][inner_index]
             product_matrix.append(inner_matrix ** 2)
         new_matrix.append(product_matrix)
