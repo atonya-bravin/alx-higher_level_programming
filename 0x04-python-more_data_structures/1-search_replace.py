218619228eab39932bb4e07fc6d4fcf706ec29c4
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-07 11:33:28 -0800

    1-search_replace.py: find and replace and element in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for index in range(len(my_list)):
+        if my_list[index] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[index])
+    return new_list
