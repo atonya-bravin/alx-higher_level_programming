31ea0ac7fe8cb774c6f5d5bba41305bb49ca195a
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-19 12:12:21 -0800

    0-safe_print_list.py: prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+        counter = 0;
+        try:
+            for index in range(x):
+                print(my_list[index], end="")
+                counter += 1
+        except TypeError:
+            pass
+        finally:
+            print()
+            return (counter)
