bc78a9c3ff96761c9d0715a2ee4adf67982236d7
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-20 09:27:03 -0800

    0-square.py: creation of a class 'Square' that is empty

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Creation of an empty class Square that defines a square
+"""Creation of an empty class Square that defines a square"""
 
 class Square:
-    # Empty class Square that defines a square
+    """Empty class Square that defines a square"""
     pass
