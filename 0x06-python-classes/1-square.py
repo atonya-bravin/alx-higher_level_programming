720ea052edbed7c25d0770d4dc825010095b466a
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-20 11:15:29 -0800

    1-square.py: creation of a private atribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Square class
+    Creation of an empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+        This class defines a Private instance attribute: size
+    """
+
+    def __init__(self, size):
+        """initializer function"""
+
+        self.__size = size
