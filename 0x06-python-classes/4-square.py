2ce3fe0974dfe86fe27ddca042d320b4cfa7e95d
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-20 17:59:06 -0800

    4-square.py: creates a setter and a getter of the size property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""Square class
+   Creation of an empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+        This class defines a Private instance attribute: size
+    """
+
+    def __init__(self, size=0):
+        """initializer function"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrives the size of the square"""
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square (setter)"""
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates and returns the area of a square"""
+
+        return (self.__size ** 2)
