b4a1d5532c9dea1a8af58f5376d8580b73c24f03
bravin-atonya <bravinatonya.brani@gmail.com>
2022-12-27 19:41:33 -0800

    4-list_division.py: division of two list elements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,9 +5,9 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for index in range(list_length):
         try:
-            if ((type(my_list_1[index]) is int) or ((type(my_list_1[index]) is float)) and \
-                ((type(my_list_2[index]) is int) or (type((my_list_2[index] is float))))):
-                result = my_list_1[index] / my_list_2[index]
+            if ((type(my_list_1[index]) is int or float)):
+                if ((type(my_list_2[index]) is int or float)):
+                    result = my_list_1[index] / my_list_2[index]
             else:
                 raise(TypeError)
         except (IndexError):
