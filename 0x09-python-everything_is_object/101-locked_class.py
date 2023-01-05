c06e1a13c7c58ea5a4549851f1ef8fce8b98b55b
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-05 01:37:08 -0800

    101-locked_class.py: locked to create only 'first_name' instances

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+"""
+    Module with a locked class
+"""
+
+
 class LockedClass(object):
+    """
+        This class instances can only and only take the property 'first_name'
+    """
+
     __slots__ = 'first_name'
