017d409303727c53017344f8776bfa91d57755fb
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-05 18:41:17 -0800

    9-rectangle.py: build up on 8-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -123,7 +123,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size) 
+        return cls(size, size)
 
     def __init__(self, width=0, height=0):
         """
