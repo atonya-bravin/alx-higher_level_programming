c366bbef6c1ed52130c627d38b553c66d89b9cf2
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-09 04:13:16 -0800

    1-my_list.py: prints a sorted list. 1-my_list.txt: test case for 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module that prints a sorted list
+"""
+
+
+class MyList(list):
+    """
+        the class that is used to hold the list data
+    """
+
+    def print_sorted(self):
+        """
+            the method that prints the sorted list
+        """
+
+        if issubclass(MyList, list):
+            print(sorted(self))
+
+
