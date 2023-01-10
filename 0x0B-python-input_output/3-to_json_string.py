29aab5610b12b154d40b9c743f8dde5de7b7b3ea
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-10 04:25:38 -0800

    3-to_json_string.py: returns the object(string) representation of JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+"""
+    Module that is tasked with JSON rep as object(string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+        returns JSON rep as object(string)
+    """
+
+    return json.dumps(my_obj)
