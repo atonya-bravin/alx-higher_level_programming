3700ac2ef2a92af5b22ef427ac4302ebf8d222b1
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-11 09:36:49 -0800

    100-my_int.py: overrides == and != operators, returning the opposite of what they are supposed to return

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class MyInt(int):
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        if (self.value == other):
+            return False
+        return True
+
+    def __ne__(self, other):
+        if (self.value != other):
+            return False
+        return True
