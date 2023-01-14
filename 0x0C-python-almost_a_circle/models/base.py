df9e3e7310138ee2ad268f6eda57fe36003462b2
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-14 09:31:34 -0800

    base class in models directory

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    This is the base mode
+"""
+
+
+class Base():
+    """
+        The goal of this class is to manage id attribute in all our future
+        classes and to avoid duplicating the same code (by extension, same bugs)
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if (id != None):
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
