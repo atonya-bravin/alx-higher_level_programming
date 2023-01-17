314729f543aea0b90dc2d6dda9682aca43565b39
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-16 17:11:34 -0800

    models/rectangle.py: method documentations

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,10 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            initialization method
+        """
+
         super().__init__(id)
 
         self.setter_validator(height, 'height')
@@ -26,6 +30,11 @@ class Rectangle(Base):
         self.__y = y
 
     def setter_validator(self, value, param):
+        """
+            validates a value if the value is a correct value
+            to be used in the application
+        """
+
         if type(value) != int:
             raise TypeError("{} must be an integer".format(param))
         if param in ('height', 'width') and value <= 0:
@@ -35,40 +44,80 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+            the width getter method
+        """
+
         return self.__width
 
     @property
     def height(self):
+        """
+            the hight getter method
+        """
+
         return self.__height
 
     @property
     def x(self):
+        """
+            the X getter method
+        """
+
         return self.__x
 
     @property
     def y(self):
+        """
+            the y getter method
+        """
+
         return self.__y
 
     @width.setter
     def width(self, width):
+        """
+            the width setter method
+        """
+
         self.__width = width
 
     @height.setter
     def height(self, height):
+        """
+            the height setter method
+        """
+
         self.__height = height
 
     @x.setter
     def x(self, x):
+        """
+            the x setter method
+        """
+
         self.__x = x
 
     @y.setter
     def y(self, y):
+        """
+            the y setter method
+        """
+
         self.__y = y
 
     def area(self):
+        """
+            finds and returns the area of a rect
+        """
+
         return (self.__width*self.__height)
 
     def display(self):
+        """
+            displays a rect using the '#' character
+        """
+
         for y_index in range(self.__y):
             print()
         for outer_index in range(self.__height):
@@ -79,6 +128,10 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
+        """
+            function overloading
+        """
+
         my_string = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                                                             self.__y,
                                                             self.__width,
@@ -86,6 +139,10 @@ class Rectangle(Base):
         return my_string
 
     def update(self, *args, **kwargs):
+        """
+            uses passed in parameters to updarte class attributes
+        """
+
         modif_attrs = ['id', 'width', 'height', 'x', 'y']
 
         argument_count = len(args)
