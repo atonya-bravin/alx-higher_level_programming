cbe79e45e6d81234b12e2dffc604aacdc49b3796
bravin-atonya <bravinatonya.brani@gmail.com>
2023-01-19 09:18:45 -0800

    square.py: adds the to_dictionary function to the square class that returns a dictionary of the square attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -57,3 +57,11 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if key in valid_args:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+                    'id': self.id,
+                    'size': self.size,
+                    'x': self.x,
+                    'y': self.y
+                }
