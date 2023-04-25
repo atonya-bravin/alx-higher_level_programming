9f197d79f4f8c6e7a9d7a238a87428c481139657
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-25 20:37:51 +0000

    script that writes contents to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -9,7 +9,7 @@ if (process.argv.length < 4) {
 const fs = require('fs');
 const filename = process.argv[2];
 const string = process.argv[3];
-fs.readFile(filename, 'utf8', string, (err) => {
+fs.readFile(filename, string, {encoding: 'utf8'}, (err) => {
 	if (err) {
 		console.log(err);
 		process.exit(1);
