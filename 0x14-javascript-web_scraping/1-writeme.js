b641b43c64a74c9b81a50302301ca2ab663cd880
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-25 20:33:15 +0000

    script that writes contents to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 // Check if the user entered 2 arguments in the shell
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+if (process.argv.length < 4) {
+  console.log('Usage: node ' + process.argv[1] + ' FILENAME INPUT_STRING');
   process.exit(1);
 }
 // Call on the filesystem
 const fs = require('fs');
-const filename = process.argv[2];
-fs.readFile(filename, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  console.log(data);
+const filename = process.argv[3];
+fs.readFile(filename, 'utf8', string, (err) => {
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
+	console.log(data);
 });
