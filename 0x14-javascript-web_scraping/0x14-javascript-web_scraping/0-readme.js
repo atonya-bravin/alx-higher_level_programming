97e884617b2150ab5b5b5b485a34754013f968db
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-25 15:05:42 +0000

    script that reads the contents of a file

--- 0x14-javascript-web_scraping/0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0x14-javascript-web_scraping/0-readme.js
@@ -10,6 +10,5 @@ var fs = require('fs');
 var filename = process.argv[2];
 fs.readFile(filename, 'utf8', function(err, data) {
     if (err) throw err;
-    console.log('OK: ' + filename);
     console.log(data)
 });
