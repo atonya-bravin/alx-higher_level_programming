bc9279dac9ac78b24d1b07fd2ec177d89051875b
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-25 15:01:04 +0000

    script that reads the contents of a file

---
+++ 0x14-javascript-web_scraping/0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Check if the user entered 2 arguments in the shell
+if (process.argv.length < 3) {
+	console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+	process.exit(1);
+}
+// Call on the filesystem
+var fs = require('fs');
+var filename = process.argv[2];
+fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    console.log(data)
+});
