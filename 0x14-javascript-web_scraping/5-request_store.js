bb7a1846d3da801559e8a7ebee9a781f66986999
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-26 01:15:58 +0000

    script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Check if the user entered 2 arguments in the shell
+if (process.argv.length < 4) {
+  console.log('Usage: node ' + process.argv[1] + ' FILENAME INPUT_STRING');
+  process.exit(1);
+}
+// Call on the filesystem
+const fs = require('fs');
+const filename = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(filename, string, { encoding: 'utf-8' }, (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
