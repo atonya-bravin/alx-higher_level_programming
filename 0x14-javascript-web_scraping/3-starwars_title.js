2d92a76f282d041d06230857ae07b65b74ae8e68
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-26 01:03:18 +0000

    script that prints the title of a Star Wars movie where the episode number matches a given integer.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = process.argv[2];
-let times = 0;
 
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body).results;
+// Get the movie ID from the command-line arguments
+const movieId = process.argv[2];
 
-  for (let i = 0; i < body.length; ++i) {
-    const characters = body[i].characters;
-
-    for (let j = 0; j < characters.length; ++j) {
-      const character = characters[j];
-      const characterId = character.split('/')[5];
-
-      if (characterId === '18') {
-        times += 1;
-      }
-    }
+// Make a GET request to the Star Wars API with the movie ID
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
+// Check for errors or non-200 status code
+  if (error || response.statusCode !== 200) {
+    console.error(`Error fetching movie data: ${error || response.statusCode}`);
+    return;
   }
-
-  console.log(times);
+  // Parse the movie data from the response body
+  const movie = JSON.parse(body);
+  // Check if the episode number matches the given ID
+  if (movie.episode_id === parseInt(movieId)) {
+    console.log(`Title: ${movie.title}`);
+  } else {
+    console.log(`No movie found with ID ${movieId}`);
+  }
 });
