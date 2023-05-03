8c997562830e910a2a8a68b1ce9c918573847c44
= <=>
2023-05-03 17:16:04 +0000

    script that changes text color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () =>{
+	function changeTextColor()
+	{
+		let my_header = document.querySelector('header');
+		my_header.style.color = '#FF0000';
+	}
+	changeTextColor();
+});
