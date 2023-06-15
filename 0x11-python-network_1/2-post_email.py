af58db111e8d208971eb408152bccaf2d8100fff
= <=>
2023-06-15 22:00:10 +0000

    script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
                     }).encode('ascii')
     req = Request(argv[1], data)
 
-    with urlopen(req) as res:
+    pt that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in
+             utf-8)" && git pushwith urlopen(req) as res:
         print(res.read().decode('utf-8'))
